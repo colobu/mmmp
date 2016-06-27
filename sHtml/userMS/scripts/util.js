69b4d905f3b2a431f6bd4f56dbcf2956c753f55f
kolaincywe <kolaincywe@163.com>
2016-06-27 18:09:34 +0800

    添加课程管理页及详情页
    添加课程管理页（course）
    添加详情页（detail）

--- sHtml/userMS/scripts/util.js
+++ sHtml/userMS/scripts/util.js
@@ -1,7 +1,24 @@
 $(function(){
-	
-
 	loginResize();
+
+	$('.rePass').on('click',function(){
+		$('.popMask').show();
+		$('.popRepass').show();
+	})
+	$('.icons-close').on('click',function(){
+		$('.popMask').hide();
+		$(this).parents('.pop').hide();
+	})
+
+	$('.goNote').on('click',function(){
+		$('.popMask').show();
+		$('.popNote').show();
+	})
+
+	$('.goState').on('click',function(){
+		$(this).toggleClass('over');
+	})
+
 })
 
 function loginResize(){
@@ -26,8 +43,18 @@ $(window).on('resize',function(){
 
 
 // 首页
-function addMe() {
-    url = document.URL;  //你自己的主页地址
+function addFavorite() {
+	url = document.URL;  //你自己的主页地址
     title = "七彩部落管理平台";  //你自己的主页名称
-    window.external.AddFavorite(url, title);
+	try {
+		window.external.addFavorite(url, title);
+	}
+	catch (e) {
+		try {
+			window.sidebar.addPanel(title, url, "");
+		}
+		catch (e) {
+			alert("抱歉，您所使用的浏览器无法完成此操作。\n\n加入收藏失败，请使用Ctrl+D进行添加");
+		}
+	}
 }
